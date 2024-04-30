c02395f1aaf68bb49e23a010558dbdad39f91100
kirayagami66 <144526305+kirayagami66@users.noreply.github.com>
2024-04-30 20:59:10 +0530

    Add files via upload

---
+++ Untitled1-MSI.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int a=3, b=4;
+	if(a>b)
+	{
+		printf("a is greatest");
+	}
+	else if (a==b)
+	{
+		printf("Both are equal");
+	}
+	if(a<b)
+	{
+		printf("b is greatest");
+	}
+	return 0;
+	
+}
+	
+	
